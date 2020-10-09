4f8171a1261ffb33ee9dea673fa5a2e9a0cfc6dc
jmutt78 <jmcintosh@docusites.com>
2020-10-09 15:24:50 -0700

    aded backend

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -3,10 +3,36 @@ import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 // Then we give our schema to the builder and provide the result to Sanity
+
+import blockContent from './blockContent';
+import blockText from './blockText';
+import category from './category';
+import figure from './figure';
+import mainImage from './mainImage';
+import post from './post';
+import postAuthor from './postAuthor';
+import slideshow from './slideshow';
+import person from './person';
+
 export default createSchema({
   // We name our schema
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([
+    // The following are document types which will appear
+    // in the studio.
+    blockContent,
+    blockText,
+    category,
+    figure,
+    mainImage,
+    person,
+    post,
+    postAuthor,
+    slideshow,
+
+    // When added to this list, object types can be used as
+    // { type: 'typename' } in other document schemas
+  ]),
 });
